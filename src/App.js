de503051fa2c828896832485a39eefd8ada39809
Tornike Khatiashvili <100285138+toko-cheetah@users.noreply.github.com>
2022-07-06 20:34:24 +0400

    Personal Info page validation added

--- src/App.js
+++ src/App.js
@@ -8,17 +8,60 @@ import PersonalInfo from "./Components/PersonalInfo";
 
 function App() {
   const [data, setData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    date_of_birth: "",
-    experience_level: "",
-    already_participated: "",
-    character_id: "",
+    name: storageData("name"),
+    email: storageData("email"),
+    phone: storageData("phone"),
+    date_of_birth: storageData("date_of_birth"),
+    experience_level: storageData("experience_level"),
+    already_participated: storageData("already_participated"),
+    character_id: storageData("character_id"),
   });
 
+  const validate = {
+    name: {
+      isValid: data.name.length > 1,
+      message: "Name should be minimum 2 characters long!",
+    },
+    email: {
+      isValid:
+        data.email.slice(-"@redberry.ge".length) === "@redberry.ge" &&
+        data.email.length > "@redberry.ge".length,
+      message: "Email should be <your email>@redberry.ge",
+    },
+    phone: {
+      isValid: data.phone.length === 9 && !/\D/.test(data.phone),
+      message: "Phone should contain 9 number characters",
+    },
+    date_of_birth: {
+      isValid: data.date_of_birth,
+      message: "Birth date is required!",
+    },
+    experience_level: {
+      isValid: data.experience_level,
+      message: "Experience level is required!",
+    },
+    already_participated: {
+      isValid: data.already_participated,
+      message: "Participation information is required!",
+    },
+    character_id: {
+      isValid: data.character_id,
+      message: "Character is required!",
+    },
+  };
+
+  const [isFilling, setIsFilling] = useState({
+    background: "none",
+    border: "1px solid #e5e6e8",
+  });
+  // setIsFilling({ background: "#E9FAF1", border: "none" })
+
   sessionStorage.setItem("data", JSON.stringify(data));
-  // sessionStorage.clear();
+
+  function storageData(el) {
+    const dat = JSON.parse(sessionStorage.getItem("data"));
+    return dat ? dat[el] : "";
+  }
 
   function onChange(event) {
     const e = event.target;
@@ -38,7 +81,14 @@ function App() {
           <Route exact path="/" element={<GetStarted />} />
           <Route
             path="/personal-info"
-            element={<PersonalInfo onChange={onChange} />}
+            element={
+              <PersonalInfo
+                onChange={onChange}
+                data={data}
+                isFilling={isFilling}
+                validate={validate}
+              />
+            }
           />
         </Routes>
       </div>
--- src/Components/PersonalInfo.js
+++ src/Components/PersonalInfo.js
@@ -1,25 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function PersonalInfo({ onChange }) {
+export default function PersonalInfo({ onChange, data, isFilling, validate }) {
+  const [type, setType] = useState("text");
+  const navigate = useNavigate();
+
+  function validation() {
+    !validate.name.isValid && alert(validate.name.message);
+    !validate.email.isValid && alert(validate.email.message);
+    !validate.phone.isValid && alert(validate.phone.message);
+    !validate.date_of_birth.isValid && alert(validate.date_of_birth.message);
+
+    return (
+      validate.name.isValid &&
+      validate.email.isValid &&
+      validate.phone.isValid &&
+      validate.date_of_birth.isValid &&
+      navigate("/chess-experience")
+    );
+  }
+
   return (
-    <div>
+    <div id="personal-info">
       <header>
         <p>Start creating your account</p>
       </header>
 
       <main>
+        <div className="wizard">
+          <div>
+            <div className="wizard-1" style={isFilling}>
+              <p>1</p>
+            </div>
+
+            <p>Personal information</p>
+          </div>
+
+          <div>
+            <div className="wizard-2">
+              <p>2</p>
+            </div>
+
+            <p>Chess experience</p>
+          </div>
+        </div>
+
         <div className="heading">
           <h2>Personal information</h2>
           <p>This is basic informaton fields</p>
         </div>
 
-        <form>
+        <form id="form-personal-info">
           <input
             type={"text"}
             id="name"
             placeholder="Name *"
             onChange={onChange}
+            value={data.name}
             required
           />
           <input
@@ -27,6 +64,7 @@ export default function PersonalInfo({ onChange }) {
             id="email"
             placeholder="Email address *"
             onChange={onChange}
+            value={data.email}
             required
           />
           <input
@@ -34,13 +72,17 @@ export default function PersonalInfo({ onChange }) {
             id="phone"
             placeholder="Phone number *"
             onChange={onChange}
+            value={data.phone}
             required
           />
           <input
-            type={"date"}
+            type={type}
+            onFocus={() => setType("date")}
+            onBlur={() => setType("text")}
             id="date_of_birth"
             placeholder="Date of birth *"
             onChange={onChange}
+            value={data.date_of_birth}
             required
           />
         </form>
@@ -50,7 +92,9 @@ export default function PersonalInfo({ onChange }) {
             <button className="back">Back</button>
           </Link>
 
-          <button className="next">Next</button>
+          <button className="next" onClick={validation}>
+            Next
+          </button>
         </div>
       </main>
     </div>
